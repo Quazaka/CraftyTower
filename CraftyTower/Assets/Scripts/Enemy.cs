74dbcb61333ff84b185c148f6fb793e6001565a3
Mike <keaq92@gmail.com>
2016-06-24 16:14:57 +0200

    Moving enemies

--- CraftyTower/Assets/Scripts/Enemy.cs
+++ CraftyTower/Assets/Scripts/Enemy.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour, Damage {
 
-    public GameObject Tower;
+    private GameObject Tower;
+    private Vector3 towerPos;
+
     int hp = 100;
+    public float moveSpeed = 5f;
 
 	// Use this for initialization
 	void Start ()
     {
-	
-	}
+        Tower = GameObject.FindGameObjectWithTag("Tower");
+        towerPos = Tower.transform.position;
+        towerPos.y -= (Tower.transform.localScale.y / 2);
+        towerPos.y += (this.transform.localScale.y / 2);
+    }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
-        
+        Move();
 	}
 
     public int damage
@@ -26,7 +32,20 @@ public class Enemy : MonoBehaviour, Damage {
     //Take damage from bullet
     void TakeDamage(int damage)
     {
-
         hp = hp - damage;
+    }
+
+    void Move()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, towerPos, moveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider co)
+    {
+        if( co.name == "Tower" )
+        {
+            //Tower.TakeDamage();
+            Destroy(gameObject);
+        }
     }
 }
