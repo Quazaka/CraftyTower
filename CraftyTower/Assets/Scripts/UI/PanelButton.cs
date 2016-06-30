06844ffe9589b11e9ff94a4f7ee38cb980a5d4d8
Mike <keaq92@gmail.com>
2016-06-30 13:56:38 +0200

    Beginning UI
    Basically just visual, no point in having the Canvas enabled.
    Also made Resources folder for when using Resources.LoadAll( )
    Font and Textures are just for fun(slight visual) right now.

---
+++ CraftyTower/Assets/Scripts/UI/PanelButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelButton : MonoBehaviour {
+
+    // Toggle the panel on/off
+    // TODO: Add a button that opens the panel - add this script
+	public void TogglePanel(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
---
+++ CraftyTower/Assets/Scripts/UI/WeaponChooser.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class WeaponChooser : MonoBehaviour
+{
+    // Script for the DropDown menu when choosing weapon to spawn
+    // TODO: Make it possible to create a weapon from the chosen option
+    // TODO: Make it such that list is only filled with weapons possible to create (e.g. some are unlocked after clearing wave XX)
+    // TODO: Update list when new weapons are unlocked.
+
+    public Dropdown dropDown;
+    public List<string> AvaiableWeapons = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+        dropDown.ClearOptions();
+
+        // Load all prefabs in Weapons folder, but only add the weapon prefabs (not projectile etc.)
+        var wepPrefabs = Resources.LoadAll("Prefabs/Weapons");
+
+        foreach (GameObject wep in wepPrefabs)
+        {
+            if (wep.tag == "Weapon")
+            {
+                AvaiableWeapons.Add(wep.name);
+            }
+        }
+        dropDown.AddOptions(AvaiableWeapons);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
