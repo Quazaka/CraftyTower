7a5f6a21d9726c88d649628ac6d07d796391b767
Mathias Corlin Mikkelsen <mcmi13@student.aau.dk>
2016-07-02 17:40:58 +0200

    Okay måske heller ikke så sjovt igen

--- CraftyTower/Assets/Scripts/Tower.cs
+++ CraftyTower/Assets/Scripts/Tower.cs
@@ -27,7 +27,8 @@ public class Tower : MonoBehaviour, IDamage {
 	// Update is called once per frame
 	void Update ()
     {
-
+        transform.Rotate(Vector3.right, Time.deltaTime + 0.25f);
+        transform.Rotate(Vector3.up, Time.deltaTime + 0.25f, Space.World);
     }
 
     // From Damage interface
