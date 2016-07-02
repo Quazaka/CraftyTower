fd53fb935e71a72b78986a9ab5db9880fc55fffb
Mike <keaq92@gmail.com>
2016-07-02 16:31:09 +0200

    Created a working healthbar

--- CraftyTower/Assets/Scripts/Tower.cs
+++ CraftyTower/Assets/Scripts/Tower.cs
@@ -5,14 +5,23 @@ using UnityEngine.UI;
 
 public class Tower : MonoBehaviour, IDamage {
 
-    // TODO: Finish healthbar and add a script to show it
+    // TODO: Make the healthbar use its own script
 
-    public float health = 100;
+    [SerializeField] // Have this field as private but force unity to show it in the editor
+    // TODO: We should practice this and use more propteries to ensure encapsulation 
+    private float startingHealth = 100;
+    private float currentHealth;
+
+    [SerializeField]
+    private Image healthContent;
+    [SerializeField]
+    private Text healthText;
 
 	// Use this for initialization
 	void Start ()
-    {
-        
+    {        
+        currentHealth = startingHealth;
+        setHealthText();
     }
 	
 	// Update is called once per frame
@@ -30,12 +39,29 @@ public class Tower : MonoBehaviour, IDamage {
     // Take damage from enemies
     private void TakeDamage(float damage)
     {
-        health -= damage;
-
+        // Lower current health based on damage input
+        currentHealth -= damage;
+        
         //Deactiavte towerobject if health is below zero
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+
+        // map the health to the sprite and set the health text
+        healthContent.fillAmount = mapHealth();
+        setHealthText();
+    }
+
+    // float value used to map the health to the fillAmount of the sprite(goes from 0 - 1)
+    private float mapHealth()
+    {
+        return currentHealth / startingHealth;
+    }
+
+    // Health is shown as text on the healthbar
+    private void setHealthText()
+    {
+        healthText.text = currentHealth + "/" + startingHealth;
     }
 }
